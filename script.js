6096b11242a77c46d0215d4e3d3faba75eb1276e
iamRishabh07 <iamrishabh799@gmail.com>
2023-06-29 12:33:51 +0530

    Clear UI State functionality added

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const itemFilter = document.getElementById('filter');
 const clearBtn = document.getElementById('clear');
+// const items = itemList.querySelectorAll('li');
 
 function addItem(e) {
   e.preventDefault();
@@ -19,7 +21,10 @@ function addItem(e) {
   const button = createButton('remove-item btn-link text-red');
   li.appendChild(button);
 
+  // Add li to the DOM
   itemList.appendChild(li);
+  checkUI();
+
   itemInput.value = '';
 }
 
@@ -39,13 +44,30 @@ function createIcon(classes) {
 
 function removeItem(e) {
   if (e.target.parentElement.classList.contains('remove-item')) {
-    e.target.parentElement.parentElement.remove();
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+
+      checkUI();
+    }
   }
 }
 
 function clearAllItems(e) {
   while (itemList.firstChild) {
     itemList.removeChild(itemList.firstChild);
+  }
+  checkUI();
+}
+
+function checkUI() {
+  const items = itemList.querySelectorAll('li');
+  console.log(items);
+  if (items.length === 0) {
+    clearBtn.style.display = 'none';
+    itemFilter.style.display = 'none';
+  } else {
+    clearBtn.style.display = 'block';
+    itemFilter.style.display = 'block';
   }
 }
 
@@ -54,3 +76,5 @@ clearBtn.addEventListener('click', clearAllItems);
 itemList.addEventListener('click', removeItem);
 
 itemForm.addEventListener('submit', addItem);
+
+checkUI();
